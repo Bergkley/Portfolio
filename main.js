c3a4a00f4e02dd3a3b922508a244bd6b40cf34eb
Bergkley Brasil <bergkley.brasil@g4flex.com.br>
2024-06-29 17:12:18 -0300

    feat: new navbar , new mode-color

--- main.js
+++ main.js
@@ -11,3 +11,79 @@ var typed = new Typed(".text", {
 });
 
 
+
+document.addEventListener("DOMContentLoaded", function() {
+  const navbarLinks = document.querySelectorAll('.navbar a');
+
+  function checkSectionInView() {
+      let scrollPosition = window.scrollY + 200;
+
+      navbarLinks.forEach(link => {
+          const sectionId = link.getAttribute('href').substring(1);
+          const section = document.getElementById(sectionId);
+
+          if (section) {
+              const sectionTop = section.offsetTop;
+              const sectionHeight = section.clientHeight;
+
+              if (scrollPosition >= sectionTop && scrollPosition < (sectionTop + sectionHeight)) {
+                  navbarLinks.forEach(link => link.classList.remove('active'));
+                  link.classList.add('active');
+              }
+          }
+      });
+
+      const pageHeight = document.body.clientHeight;
+      const windowHeight = window.innerHeight;
+      if (scrollPosition >= pageHeight - windowHeight) {
+          navbarLinks.forEach(link => link.classList.remove('active'));
+          document.querySelector('.navbar a[href="#contact"]').classList.add('active');
+      }
+  }
+
+  window.addEventListener('load', checkSectionInView);
+  window.addEventListener('scroll', checkSectionInView);
+});
+
+
+
+
+
+
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+const sunIcon = document.querySelector('.bxs-sun');
+const moonIcon = document.querySelector('.bxs-moon');
+
+themeToggle.addEventListener('click', function() {
+    body.classList.toggle('dark-theme');
+    
+    // Atualizar ícones com base no tema atual
+    const isDarkMode = body.classList.contains('dark-theme');
+    if (isDarkMode) {
+        sunIcon.style.display = 'inline-block';
+        moonIcon.style.display = 'none';
+    } else {
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = 'inline-block';
+    }
+    
+    // Salvar preferência do usuário no localStorage
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+});
+
+// Verificar o tema preferido do usuário ao carregar a página
+document.addEventListener('DOMContentLoaded', function() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        body.classList.add('dark-theme');
+        sunIcon.style.display = 'inline-block';
+        moonIcon.style.display = 'none';
+    } else {
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = 'inline-block';
+    }
+});
+
+
+
